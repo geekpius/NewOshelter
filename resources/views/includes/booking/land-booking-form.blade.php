3f95b9edeaff735daefda1f7952926cd8f05bb8b
Fiifi Pius T.K <fiifipius@gmail.com>
2021-11-23 22:06:42 +0000

    added indenture disclaimer on booking land

--- resources/views/includes/booking/land-booking-form.blade.php
+++ resources/views/includes/booking/land-booking-form.blade.php
@@ -1,4 +1,17 @@
 
+
+@if(!empty($property->propertyLandDetail->indenture_file))
+    <div class="alert alert-secondary border-0" role="alert">
+        This property is have indenture uploaded. <a target="_blank" href="{{ asset('assets/images/indenture/'.$property->propertyLandDetail->indenture_file) }}">View Indenture</a>
+    </div>
+@else
+    <p class="text-danger">
+        The owner of this property has not provided indenture or any documentation of it. There are some genuine lands without indenture. Some of these lands are family lands, stools lands etc.
+        In most cases indenture is produced for the buyer after the property is purchased. OShelter can verify the identity of the listener but can not verify the authenticity of the land and will not be directly or indirectly liable to any issues that may arise from paying for the property without doing background checks.
+        DO NOT MAKE ANY PAYMENT WITHOUT VISITING THE PROPERTY. Insist on indenture before making any payment for any property you view here on OShelter.
+    </p>
+@endif
+
 <form class="mt-4" id="formLand" method="POST" action="{{ route('property.request.land') }}">
     @csrf
     <input type="hidden" name="property_id" value="{{ $property->id }}" readonly>
